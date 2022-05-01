6a8b3880ee36ba2daba3f6bfc0fcd2473031783b
ramshad-sha <100073235+ramshad-sha@users.noreply.github.com>
2022-05-01 09:15:00 +0400

    📢 Shahmwol Version 3.0 Available Now! …

--- plugins/carbon.js
+++ plugins/carbon.js
@@ -7,7 +7,7 @@ const Asena = require('../events');
 const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
 const axios = require('axios');
 const Config = require('../config');
-const dd = "ᴍᴀᴅᴇ ʙʏ ᴘʀɪɴᴄᴇʀᴜᴅʜ"
+const dd = "ᴍᴀᴅᴇ ʙʏ ʀᴀᴍsʜᴀᴅsʜᴀ"
 
 const Language = require('../language');
 const Lang = Language.getString('weather');
